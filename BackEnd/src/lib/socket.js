e425414a30a8d49e26dc4855bd7eff9d89755be7
yash <ysiddhapura6@gmail.com>
2025-07-06 12:38:52 +0530

    socket

--- BackEnd/src/lib/socket.js
+++ BackEnd/src/lib/socket.js
@@ -7,7 +7,7 @@ const server = http.createServer(app)
 const io = new Server(server,
     {
         cors: {
-            origin: ['http://localhost:5173', "http://localhost:5174"]
+            origin: ['https://chat-application-s8vu.onrender.com']
         }
     }
 )
