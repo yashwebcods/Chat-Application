0545bb330d55e280ff67e2ec6753c246ef962122
yash <ysiddhapura6@gmail.com>
2025-07-06 12:30:55 +0530

    axios

--- FrontEnd/src/lib/axios.js
+++ FrontEnd/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 export const axiosInstance = axios.create({
-    baseURL: "http://localhost:8001/api" ,
+    baseURL: "https://chat-backend-xyz.onrender.com/api" ,
     withCredentials:true
 })
